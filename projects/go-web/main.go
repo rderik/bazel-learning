854661f1c895dfd445bee18891f783e9c428d771
Derik Ramirez <derik@rderik.com>
2023-03-22 09:10:06 -0700

    Include go dependencies

--- projects/go-web/main.go
+++ projects/go-web/main.go
@@ -2,17 +2,15 @@ package main
 import (
   "log"
   "fmt"
-  "net/http"
   "math/rand"
 
   "github.com/gin-gonic/gin"
   "github.com/rderik/bazel-learning/projects/go-libs/calculator"
 )
 
-func WebHandler(w http.ResponseWriter, r * http.Request) {
+func WebHandler(c *gin.Context) {
   log.Println("Received request")
-  w.WriteHeader(http.StatusOK)
-  fmt.Fprintf(w, "Random sum: %d\n", calculator.Add(rand.Intn(100), rand.Intn(100)))
+  c.Data(200, "application/json; charset=utf-8", []byte( fmt.Sprintf("Random sum: %d\n", calculator.Add(rand.Intn(100), rand.Intn(100)))))
 }
 
 func main() {
